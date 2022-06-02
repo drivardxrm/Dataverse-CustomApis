0778eede46385557da23c417c1b1c70851d31456
Jordi <jordi.montana@gmail.com>
2022-06-02 21:26:15 +0200

    Downgrade to net462 and remove unncessary usings

--- Driv.CustomApis.Tests/API/GetEnvironmentVariableTests.cs
+++ Driv.CustomApis.Tests/API/GetEnvironmentVariableTests.cs
@@ -1,16 +1,10 @@
 ﻿using Driv.CustomApis.API;
 using FakeXrmEasy;
-using FakeXrmEasy.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Messages;
-using Microsoft.Xrm.Sdk.Metadata;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace Driv.CustomApis.Tests.API
